81056dccfad21e84e88780b84cd149754bba0f28
comatrix <comatrix70@gmail.com>
2024-08-17 11:15:08 +0800

    add some translation

--- src/components/View/CurrentlyServingQueue.tsx
+++ src/components/View/CurrentlyServingQueue.tsx
@@ -1,6 +1,7 @@
 import { Box, Heading, Flex } from "@chakra-ui/react";
 import { getQueueName, getQueueNumber } from "../../utils";
 import { ITrelloList } from "../../model";
+import useTranslation from "next-translate/useTranslation";
 
 type Props = {
   missedQueues: ITrelloList[];
@@ -8,14 +9,15 @@ type Props = {
 
 export const CurrentlyServingQueue = ({ missedQueues }: Props) => {
   console.log("CurrentlyServingQueue() missedQueues: ", missedQueues);
+  const { t, lang } = useTranslation("common");
   return (
     <Box mx={20} my={10}>
       <Flex justifyContent="space-between">
         <Heading textStyle="display1" fontSize="5xl" mb="0.5em" flex={1}>
-          Counter / Room
+          {t("server")}
         </Heading>
         <Heading textStyle="display1" fontSize="5xl" mb="0.5em" flex={1}>
-          Currently serving
+          {t("serving")}
         </Heading>
       </Flex>
 
